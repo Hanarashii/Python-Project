1c0eeb4ab109a09e00f1e6a16c091aa733801baa
Huy Le <vogiahuy2105@gmail.com>
2025-12-28 05:24:17 +0700

    init project

---
+++ src/data_ingestor/__init__.py
@@ -0,0 +1,12 @@
+import os
+import kagglehub
+
+class DataIngestor:
+    def __init__(self, path):
+        self.path = path
+    def data_downloader(self):
+        if not os.path.exists("./downloads"):
+            os.mkdir("./downloads")
+        os.environ["KAGGLEHUB_CACHE"] = "./downloads"
+        path = kagglehub.dataset_download(self.path)
+        print(f"Dataset downloaded: {path}")
