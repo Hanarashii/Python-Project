b77b18dd87b2815406815014e88da86f9ccc5ea6
Huy Le <vogiahuy2105@gmail.com>
2026-01-04 12:33:58 +0700

    Added Data cleaner

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from src.user_interface import App
 from src.data_ingestor import DataIngestor
 from src.data_loader import load_csv_dataframe
-from src.data_cleaner import CarDataCleaner
+from src.data_cleaner.Validate import validate
+from src.data_cleaner.Clean import clean_data
+from src.data_cleaner.Transform import transform_data
+from src.data_visualizer.visualizer import run_all
 
 def main():
     #This part handles download data
     kaggle_path = "nehalbirla/vehicle-dataset-from-cardekho"
     ingestor = DataIngestor(kaggle_path)
     ingestor.data_downloader()
+
+    #Load data and validate it
+    download_path = f"./downloads/datasets/{kaggle_path}"
+    raw_data = load_csv_dataframe(download_path)
+    validate(raw_data)
+
+    #Clean and transform the data
+    cleaned_data = clean_data(raw_data)
+    transform_data(cleaned_data)
+    run_all(cleaned_data)
+
 
 if __name__ == '__main__':
     main()
--- src/data_ingestor/__init__.py
+++ src/data_ingestor/__init__.py
@@ -7,6 +7,8 @@ class DataIngestor:
     def data_downloader(self):
         if not os.path.exists("./downloads"):
             os.mkdir("./downloads")
-        os.environ["KAGGLEHUB_CACHE"] = "./downloads"
-        path = kagglehub.dataset_download(self.path)
-        print(f"Dataset downloaded: {path}")
+            os.environ["KAGGLEHUB_CACHE"] = "./downloads"
+            path = kagglehub.dataset_download(self.path)
+            print(f"Dataset downloaded: {path}")
+        else:
+            return
